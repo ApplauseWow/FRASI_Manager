f5815be99ed8430f77752eab509e8b783b632144
HolyKwok <holykwok@163.com>
2018-10-04 21:59:42 +0800

    2018-10-05 21:59 start the project

--- scalable_function.py
+++ scalable_function.py
@@ -6,10 +6,11 @@ from UI.identify_id import Ui_Identity_ID_UI
 from UI.sign_in import Ui_Sign_In_UI
 from UI.sys_option import Ui_sys_option
 
+# GUI classes
 
 class Index(QMainWindow, Ui_Index_UI):
     """
-    GUI 
+    GUI -- main window
     """
 
     def __init__(self):
@@ -24,10 +25,40 @@ class Index(QMainWindow, Ui_Index_UI):
         # self.id_window.show()
 
 
+class Identify_Id_UI(QDialog, Ui_Identity_ID_UI):
+    """
+    GUI -- identify-id window
+    """
 
-"""
-following functions are scalable and pluggable
-"""
+    def __init__(self):
+        super(Identify_Id_UI, self).__init__()
+        self.setupUi(self)
 
 
+class Sign_In_UI(QDialog, Ui_Sign_In_UI):
+    """
+    GUI -- sign-in window
+    """
+
+    def __init__(self):
+        super(Sign_In_UI, self).__init__()
+        self.setupUi(self)
+
+
+class Sys_Option_UI(QDialog, Ui_sys_option):
+    """
+    GUI -- sys-option window
+    """
+
+    def __init__(self):
+        super(Sys_Option_UI, self).__init__()
+        self.setupUi(self)
+
+
+# following functions in utilities class are scalable and pluggable
+
+class Utility(object):
+    """
+    utilities
+    """
 
